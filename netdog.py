248589e042b4287b5b93fc0845f1daa0adf7c2c2
root <root@localhost.localdomain>
2016-10-05 19:49:57 -0300

    Implemented server TCP and run_command function

--- netdog.py
+++ netdog.py
@@ -9,9 +9,9 @@ import subprocess
 listen = False
 command = False
 upload = False
-execute = ""
-target = ""
-upload_destination = ""
+execute = ''
+target = ''
+upload_destination = ''
 port = 0
 
 
@@ -40,43 +40,45 @@ Examples:
     sys.exit(0)
 
 
-def main():
+def client_handler():
+    pass
 
-    if not len(sys.argv[1:]):
-        usage()
 
-    # Set params
+def server_loop():
+    global target
+
+    if not len(target):
+        target = '0.0.0.0'
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port, ))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        # Open thread for client
+        client_thread = threading.Thread(target=client_handler,
+            args=(client_socket, ))
+        client_thread.start()
+
+
+def run_command(command):
+
+    # Remove break line
+    command = command.rstrip()
+
+    # Run command and get data of output
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hle:t:c:p:cu',
-            ['help', 'listen', 'execute', 'target', 'port', 'command',
-                'upload'])
-    except getopt.GetoptError as err:
-        print str(err)
+        output = subprocess.check_output(
+            command,
+            stderr=subprocess.STOUT,
+            shell=True
+        )
+    except:
+        output = 'Failed to execute command \r\n'
 
-    for option, argument in opts:
-        if option in ('-h', '--help'):
-            usage()
-        elif option in ('-l', '--listen'):
-            listen = True
-        elif option in ('-e', '--execute'):
-            execute = argument
-        elif option in ('-c', '--commandshell'):
-            command = True
-        elif option in ('-u', '--upload'):
-            upload_destination = argument
-        elif option in ('-t', '--target'):
-            target = argument
-        elif option in ('-p', '--port'):
-            port = int(argument)
-        else:
-            assert False, 'Unhandled option'
-
-    if not listen and len(target) and  port > 0:
-        buffer = sys.stdin.read()
-        client_sender(buffer)
-
-    if listen:
-        server_loop()
+    return output
 
 
 def client_sender(buffer):
@@ -114,6 +116,51 @@ def client_sender(buffer):
     except:
         print '[*] Exception! Exiting'
         client.close()
+
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    # Set params
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hle:t:c:p:cu',
+            ['help', 'listen', 'execute', 'target', 'port', 'command',
+                'upload'])
+    except getopt.GetoptError as err:
+        print str(err)
+
+    for option, argument in opts:
+        if option in ('-h', '--help'):
+            usage()
+        elif option in ('-l', '--listen'):
+            listen = True
+        elif option in ('-e', '--execute'):
+            execute = argument
+        elif option in ('-c', '--commandshell'):
+            command = True
+        elif option in ('-u', '--upload'):
+            upload_destination = argument
+        elif option in ('-t', '--target'):
+            target = argument
+        elif option in ('-p', '--port'):
+            port = int(argument)
+        else:
+            assert False, 'Unhandled option'
+
+    if not listen and len(target) and port > 0:
+        buffer = sys.stdin.read()
+        client_sender(buffer)
+
+    if listen:
+        server_loop()
 
 
 if __name__ == '__main__':
